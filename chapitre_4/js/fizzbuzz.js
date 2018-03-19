a1108be17e93a48364da7a3dd69ec00608a7b0ef
Laureen Perrin <laureencoupe@hotmail.fr>
2018-03-19 21:10:45 +0100

    addlast exercice in chapter 4.

---
+++ chapitre_4/js/fizzbuzz.js
@@ -0,0 +1,10 @@
+var nombre = 1;
+for (nombre = 1; nombre <= 100; nombre++) {
+    if (nombre % 3 === 0) {
+        console.log("FizzBuzz");
+    } else if (nombre % 5 === 0) {
+        console.log("FizzBuzz");
+    } else {
+    	console.log(nombre);
+    }
+}
---
+++ chapitre_4/js/triangle_while.js
@@ -0,0 +1,8 @@
+var ligne = "";
+var i = 1;
+
+while (i <= 7) {
+    ligne += "#";
+    console.log(ligne);
+    i++;
+}
